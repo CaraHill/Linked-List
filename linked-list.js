4571b91ab9803bf1600fb1a7f29eff6497076284
Cara Hill <cara@rabidtech.co.nz>
2019-03-29 14:44:48 +1300

    Broken tests. :(

--- linked-list.js
+++ linked-list.js
@@ -4,18 +4,30 @@ class LinkedList {
     this.after = null;
     this.thing = null;
     this.incremental = 0;
+    this.listItems = new Object;
   }
 
   push(number) {
+    if(this.thing != null && this.before != null) {
+      this.after = this.before;
+      this.before = this.thing;
+      this.thing = number;
+      this.incremental += 1;
+      return;
+    }
+
     if(this.thing == null) {
       this.thing = number;
+      this.incremental += 1;
+      return;
     }
 
     if(this.thing != null) {
       this.before = this.thing;
       this.thing = number;
+      this.incremental += 1;
+      return
     }
-    this.incremental += 1;
   }
 
   pop() {
@@ -38,6 +50,8 @@ class LinkedList {
   shift() {
     let firstItem;
 
+    console.log(this.before, this.after, this.thing)
+
     if(this.before != null && this.after != null) {
       firstItem = this.before;
       this.before = null;
@@ -52,17 +66,21 @@ class LinkedList {
       firstItem = this.thing;
 
       this.thing = this.after;
+      this.incremental -= 1;
+      return firstItem;
     } else if(this.before == null) {
       firstItem = this.thing;
 
       this.thing = null;
+      this.incremental -= 1;
+      return firstItem;
     } else {
       firstItem = this.before;
 
       this.before = this.thing;
+      this.incremental -= 1;
+      return firstItem;
     }
-    this.incremental -= 1;
-    return firstItem;
   }
 
   unshift(number) {
@@ -75,12 +93,37 @@ class LinkedList {
     if(this.thing != null) {
       this.after = this.thing;
       this.thing = number;
+      this.incremental += 1;
+      return;
     }
-    this.incremental += 1;
   }
 
   count() {
     return this.incremental;
+  }
+
+  delete(number) {
+    this.listItems["before"] = this.before;
+    this.listItems["after"] = this.after;
+    this.listItems["thing"] = this.thing;
+    console.log(this.listItems)
+
+    switch (Object.keys(this.listItems).find(key => this.listItems[key] === number)) {
+      case "before":
+        this.before = this.after;
+        this.after = null;
+        this.incremental -= 1;
+        break;
+      case "after":
+        this.after = this.before;
+        this.before = null;
+        this.incremental -= 1;
+        break;
+      case "thing":
+        this.thing = null;
+        this.incremental -= 1;
+    }
+    return;
   }
 }
 
